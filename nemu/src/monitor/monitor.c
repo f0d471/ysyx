732030e82956c40241d50624ffba189de7986cbe
ysyx_25030090-Zeng Guangcong <1429781970@qq.com>
2026-01-24 04:55:55 -0500

    NJU-ProjectN/am-kernels ics2021 initialized without tracing
    NJU-ProjectN/am-kernels c4088239e2425ffcb3cccd0fd603872d6c745fbb bad-apple: fix bug of channel number and audio_left

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -24,18 +24,6 @@ void init_device();
 void init_sdb();
 void init_disasm();
 
-static void welcome() {
-  Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
-  IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
-        "to record the trace. This may lead to a large log file. "
-        "If it is not necessary, you can disable it in menuconfig"));
-  Log("Build time: %s, %s", __TIME__, __DATE__);
-  printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
-  printf("For help, type \"help\"\n");
-  Log("Exercise: Please remove me in the source code and compile NEMU again.");
-  assert(0);
-}
-
 #ifndef CONFIG_TARGET_AM
 #include <getopt.h>
 
@@ -130,8 +118,6 @@ void init_monitor(int argc, char *argv[]) {
 
   IFDEF(CONFIG_ITRACE, init_disasm());
 
-  /* Display welcome message. */
-  welcome();
 }
 #else // CONFIG_TARGET_AM
 static long load_img() {
