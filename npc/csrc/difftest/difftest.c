3a649e442373f3051a14e064b50c01f0899ffdbf
f0d471 <1429781970@qq.com>
2026-05-29 03:37:43 +0800

    difftest

--- npc/csrc/difftest/difftest.c
+++ npc/csrc/difftest/difftest.c
@@ -10,28 +10,24 @@ static void (*ref_memcpy)(uint32_t addr, void *buf, size_t n, int direction) = N
 static void (*ref_regcpy)(void *dut, int direction) = NULL;
 static void (*ref_exec)(uint64_t n) = NULL;
 static void (*ref_raise_intr)(int NO) = NULL;
-static void (*ref_init)(int port) = NULL;
 static void *handle = NULL;
 
 void difftest_init(const char *so_file) {
   handle = dlopen(so_file, RTLD_NOW);
   if (!handle) {
     fprintf(stderr, "dlopen error: %s\n", dlerror());
-    npc_state = NPC_ABORT; 
+    npc_state = NPC_ABORT;
     npc_quit();  }
 
   ref_memcpy     = (void (*)(uint32_t, void*, size_t, int)) dlsym(handle, "difftest_memcpy");
   ref_regcpy     = (void (*)(void*, int))                   dlsym(handle, "difftest_regcpy");
   ref_exec       = (void (*)(uint64_t))                     dlsym(handle, "difftest_exec");
   ref_raise_intr = (void (*)(int))                          dlsym(handle, "difftest_raise_intr");
-  ref_init       = (void (*)(int))                          dlsym(handle, "difftest_init");
 
-  if (!ref_memcpy || !ref_regcpy || !ref_exec || !ref_raise_intr || !ref_init) {
+  if (!ref_memcpy || !ref_regcpy || !ref_exec || !ref_raise_intr) {
     fprintf(stderr, "dlsym error\n");
     npc_state = NPC_ABORT;
     npc_quit();  }
-
-  ref_init(0);
 
   printf("DiffTest REF loaded: %s\n", so_file);
 }
